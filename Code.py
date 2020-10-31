2606db59e780c4bb8368b3efca2ee9ee91d7c166
KavinSatheeskumar <kavinsathees@gmail.com>
2020-10-31 12:54:48 -0400

    Adding Tsai-Wu Failure criteria to the calculator
    Loop over the arrays plystresses and maxstresses to determine if the laminate will fail.

--- Code.py
+++ Code.py
@@ -105,3 +105,67 @@ for i in range(3):
     plt.title("Strain Through Laminate Thickness ")
     
 print ("forces \n",forces,"\n moments \n",moments,"\n strain \n",midstrain)
+
+## The Tsai-Wu failure Criteria for laminates.
+## If the expresion given is greater than 1 for any of the
+## layers of the laminate, the laminate will fail.
+## The expression takes as input the maximum stresses
+## The laminate can take as well as the stresses it is curretly
+## experiencing to determine if the laminate will fail.
+## [Note, I'll define all the variable names once I can
+## get an explanation as to what they all really mean]
+
+def tsai_wu(F1t, F1c, F2t, F2c, F12, s1, s2, s12):
+    f1 = 1/F1t - 1/F1c
+    f11 = 1/(F1t * F1c)
+    f2 = 1/F2t - 1/F2c
+    f22 = 1/(F2t * F2c)
+    f12 = -1/2 * (f11 * f22)**(0.5)
+    f66 = 1/(F12 ** 2)
+
+    return f1*s1 + f2*s2 + f11*s1*s1 + f22*s2*s2 + f66*s12*s12 + 2*f12*s1*s2
+
+## The following code assumes that an arrays called
+## plystresses will be created in the following form
+## plystresses = [
+##          [s1, s2, s12],
+##          [s1, s2, s12],
+##          [s1, s2, s12],
+##          ...
+## ]
+## where each element plystresses[i] of the array is the sigma 1, 2 and 12 of
+## for the ply at stack[i]
+
+## maxstresses = [
+##          [F1t, F1c, F2t, F2c, F12],
+##          [F1t, F1c, F2t, F2c, F12],
+##          [F1t, F1c, F2t, F2c, F12],
+##          ...
+## ]
+## Where each element of maxstresses[i] of the array is the
+## maximum stress that the ply at stack[i]
+
+##len(plystresses) = len(maxstresses)
+
+plystresses = np.zeros(shape=(len(height),3))
+##maxstresses can be or can not be a numpy array, it dosen't matter
+maxstresses = np.array([[1,1,1,1,1] for _ in range(len(height))])
+
+has_not_failed = True
+
+for i in range(len(plystresses)):
+    tsai_wu_value = tsai_wu(
+            maxstresses[i][0],
+            maxstresses[i][1],
+            maxstresses[i][2],
+            maxstresses[i][3],
+            maxstresses[i][4],
+            plystresses[i][0],
+            plystresses[i][2],
+            plystresses[i][3]
+        )
+
+    if (tsai_wu_value >= 1):
+        print(tsai_wu_value)
+        has_not_failed = False
+        print("There was a failure in layer " + str(i+1))
