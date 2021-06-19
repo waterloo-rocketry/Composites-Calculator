d5ff7130d1758e0974410509f551c088432b7397
Ben Dunk <ben@bidmii.com>
2021-06-19 15:18:51 -0400

    select stack appropriately

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from material import Material
+from sys import argv
+from os import path
 from stack import Stack
 import json
 
 if __name__ == "__main__":
-    with open('data/stacks/example1.json') as f:
+    if(len(argv)==1):
+        print('Please specify the file path for the stack')
+
+    stack_path = argv[1]
+    if(not path.exists(stack_path)):
+        print('Specified file does not exist')
+
+    with open(stack_path) as f:
         stack_data = json.load(f)
+
     stack = Stack(stack_data)
